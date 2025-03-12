0da2fb375c68286aaae306f4af443c15dd36ff34
aslam <aslam.ps.786@gmail.com>
2025-03-12 06:22:06 +0530

    Created main class and properties file

---
+++ src/main/java/com/myStore/rewardManagement/RewardManagementServiceApplication.java
@@ -0,0 +1,12 @@
+package com.myStore.rewardManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardManagementServiceApplication.class, args);
+    }
+}
